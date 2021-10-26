4a34cf11fcbfff3109e16fef96de97f5540a8708
Brunohermans <b.hermans@student.tudelft.nl>
2021-10-26 16:49:59 +0200

    Fixed buggy form.
    Added unique ids to each question. Also made sure

--- app/client/src/components/Datacollector.vue
+++ app/client/src/components/Datacollector.vue
@@ -2,25 +2,25 @@
   <div>
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
       <b-form-group 
-        id="input-group-2" 
+        id="input-group-1" 
         label="Wat is uw leeftijd?" 
-        label-for="input-2"
+        label-for="input-1"
         description="Omdat we van zowel jongeren als ouderen willen weten wat ze van deze plek vinden, willen we graag uw leeftijd weten. "
         >
         <b-form-input
-          id="input-2"
+          id="input-1"
           v-model="form.age"
           placeholder=""
           required
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id=input-group02 label="Hoe vaak komt u hier?" label-for="input-2">
+      <b-form-group id=input-group-2 label="Hoe vaak komt u hier?" label-for="input-2" v-slot="{ ariaDescribedby }">
         <b-form-radio-group
-        id="radio-group-1"
+        id="radio-group-2"
         v-model="form.frequency"
         :aria-describedby="ariaDescribedby"
-        name="radio-sub-component"
+        name="radio-sub-component2"
         stacked
         >
           <b-form-radio value="1" variant="primary">Minder dan één keer per maand.</b-form-radio>
@@ -32,12 +32,12 @@
 
 
       <b-form-group
-        id="input-group-1"
+        id="input-group-3"
         label="Waarom bent u hier?"
-        label-for="input-1"
+        label-for="input-3"
       >
         <b-form-select
-          id="input-1"
+          id="input-3"
           v-model="form.reasonOfVisit"
           placeholder=""
           :options="options"
@@ -50,47 +50,52 @@
     <b-form-group
       id="input-group"
       label="Hoe prettig vindt u deze plek? Geef deze plek een aantal sterren! "
-      label-for="input-2"      
+      label-for="input-4"      
       >
       <b-form-rating
-        id="input-2"
+        id="input-4"
         v-model="form.stars"
         variant="dark"
         required
         >
       </b-form-rating>
 
+    </b-form-group>
+
 
     <b-form-group
-    id="input-group-1"
+    id="input-group-5"
     label="Voelt u zich wel eens onveilig op deze plek?"
-    labelfor="input-1"
+    labelfor="input-5"
+    v-slot="{ ariaDescribedby }"
     required
     >
+    
     <b-form-radio-group
-        id="radio-group-1"
-        v-model="form.frequency"
+        id="radio-group-5"
+        v-model="form.safety"
         :aria-describedby="ariaDescribedby"
-        name="radio-sub-component"
+        name="radio-sub-component5"
         stacked
         >
-          <b-form-radio value="1" variant="primary">Ik voel me hier nooit onveilig</b-form-radio>
+          <b-form-radio value="1" variant="primary">Ik voel me hier nooit veilig</b-form-radio>
           <b-form-radio value="2">Ik heb me hier ooit een keer onveilig gevoeld</b-form-radio>
           <b-form-radio value="3">Ik voel me hier meestal veilig</b-form-radio>
           <b-form-radio value="4">Ik voel me hier altijd veilig </b-form-radio>
         </b-form-radio-group>
-    </b-form-group>
+      
+    
 
 
     </b-form-group>
       <b-form-group
-        id="input-group-1"
+        id="input-group-6"
         label="Wat kan de gemeente doen om deze plaats te verbeteren?"
-        label-for="input-1"
+        label-for="input-6"
         description="Voorbeeld: Haal vaker het afval op. De container is vaak vol."
       >
         <b-form-textarea
-          id="input-1"
+          id="input-6"
           v-model="form.feedback"
           placeholder=""
           required
@@ -116,6 +121,7 @@ import axios from 'axios'
           reasonOfVisit: '',
           stars: '',
           feedback: '',
+          safety: null,
           qr_name: '12',
         },
         options: [
