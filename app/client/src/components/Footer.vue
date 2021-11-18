a3739c20c7c04ad98dbed71cd8378edcc321451a
Brunohermans <b.hermans@student.tudelft.nl>
2021-11-18 13:04:49 +0100

    Footer

--- app/client/src/components/Footer.vue
+++ app/client/src/components/Footer.vue
@@ -8,12 +8,15 @@
                                 <div class="row no-gutters">
                                     <div class="col-1 col-md-0"></div>
                                     <div class= "col-10 col-md-12 text-center" id="subfooter1">
-                                    <h1>Citytraffic</h1>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/hoe-we-tellen/">Hoe we tellen</a></li>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/waar-tellen-we/">Waar tellen we</a></li>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/wat-tellen-we/">Wat we tellen</a></li>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/hoe-rapporteren-we/">Hoe we rapporteren</a></li>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/faqs-over-de-nieuwe-citytraffic-portal/">FAQ’s</a></li>
+                                        <div class="row no-gutters">
+                                            <div class="onsLogo col-12">
+                                                <img class="qr" src="../assets/qr.png" width="40em" ><img>
+                                                <h1 class="logoText">Challenges.Social</h1>
+                                            </div>
+                                        </div>
+                                        <div class="row mt-5"></div>
+                                       
+                                        
                                     </div>
                                     <div class="col-1 col-md-0"></div>
                                 </div>
@@ -23,10 +26,12 @@
                                 <div class="row no-gutters">
                                     <div class="col-1 col-md-0"></div>
                                     <div class= "col-10 col-md-12 text-center" id = "subfooter2">
-                                    <h1>Wat tellen we?</h1>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/wat-tellen-we/#passanten" data-et-has-event-already="true">Passanten</a></li>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/wat-tellen-we/#unieke-bezoekers" data-et-has-event-already="true">Unieke bezoekers</a></li>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/wat-tellen-we/#verblijfstijd" data-et-has-event-already="true">Verblijfstijd</a></li>
+                                    <h1>Partners</h1>
+                                     <div class="row no-gutters">
+                                            <div class="collab col-12">
+                                                <a href="https://www.denhaag.nl"><img class="denhaag" src="../assets/logo.svg" width="200em" ><img></a>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div class="col-1 col-md-0"></div>
                                 </div>
@@ -36,12 +41,9 @@
                                 <div class="row no-gutters">
                                     <div class="col-1 col-md-0"></div>
                                     <div class= "col-10 col-md-12 text-center" id = "subfooter3">
-                                    <h1>Onze privacywaarborging</h1>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/privacy-proof/">Privacy proof</a></li>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/wp-content/uploads/2018-04-10-Privacybeleid-Bureau-RMC.pdf">Privacyverklaring</a></li>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/wp-content/uploads/2020-02-06-Aanvullend-privacybeleid-Bureau-RMC-mbt-passantentellingen.pdf">Privacyprotocol passantentellingen</a></li>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/opt-out-register/">Opt-out register</a></li>
-                                    <li class = "footer_li"><a href="https://www.citytraffic.nl/wp-content/uploads/2020-Algemene-voorwaarden-Bureau-RMC.pdf">Algemene voorwaarden</a></li>
+                                    <h1>Vragen?</h1>
+                                    <li class = "footer_li"><a href="/#/contact/">Contact</a></li>
+                                    <li class = "footer_li"><a href="/#/concept/">Wat we doen?</a></li>
                                     </div>
                                     <div class="col-1 col-md-0"></div>
                                 </div>
@@ -62,13 +64,17 @@ export default {
 
 <style scoped>
 #footer{
-    background: url("../assets/home2.jpg");
+    background-color: white;
     
 }
 
 h1 {
-    color:yellow;
+    color: black;
     font-size: 1.5em;
+}
+
+a {
+    color: black;
 }
 
 .footer_li {
@@ -76,4 +82,15 @@ h1 {
     margin: 0.5em;
 }
 
+.qr {
+    float: left;
+}
+
+
+.logoText {
+    float: left;
+    margin-left: 0.4em;
+    margin-top: 0.1em;
+    font-size: 1.4em;
+}
 </style>
