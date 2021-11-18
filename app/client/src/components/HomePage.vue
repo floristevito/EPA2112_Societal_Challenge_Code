8c017335978f7cf46ae554c8f8de4451cf6b7ab0
Floris Boendermaker <floris.boendermaker@gmail.com>
2021-11-18 11:16:45 +0100

    Layout update 0.1

---
+++ app/client/src/components/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="base">
+<div id="info" class="text-center">
+  <h1 class="display-5">Welkom op de home pagina van het challanges.social project</h1>
+      <p class="lead">Middels de inzet van QR codes hopen wij jongeren te bereiken in Den Haag.</p>
+  <img loading="lazy"
+   src="../assets/home.jpg" 
+   alt="" title="qr_home" 
+   width="50%" height="50%">
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#info {
+  border-radius: 0px;
+  /* background-repeat: no-repeat; */
+}
+</style>
--- app/client/src/views/Home.vue
+++ app/client/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="home">
     <Info></Info>
+    <HomePage></HomePage>
   </div>
 </template>
 
 <script>
 import Info from '../components/Info.vue'
+import HomePage from '../components/HomePage.vue'
 
 export default {
   name: 'Home',
   components: {
-    Info
+    Info,
+    HomePage
   },
 }
 </script>
