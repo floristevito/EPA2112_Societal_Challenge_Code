bed7addb92d6f2fa241448c416d8334f7d1d1612
Brunohermans <b.hermans@student.tudelft.nl>
2021-11-18 14:25:24 +0100

    update concept

--- app/client/src/views/Concept.vue
+++ app/client/src/views/Concept.vue
@@ -1,17 +1,10 @@
 <template>
   <div id="concept">     
-      <Infostatic title="QR-interests" content="
-            Based on the findings of the literature study, young people are interested in participating in policymaking and improving the livability of their city/neighborhoods.
-            The municipality of The Hague currently fuels this interest by inviting people to the Wijktafels and through programs such as The Haagse Jongeren Ambassadeurs.
-            Although these programs are reported as successful, they employ ‘traditional’ methods of involving citizens in policy making (e.g. conversation, meetings).
-            Exactly these traditional methods are distrusted by the youth, as they do not give them a sense of empowerment. 
-            This leads to an underrepresentation of youth in decision-making processes.
-            This problem of underrepresentation can perhaps be alleviated by employing innovative methods that use low-barrier of entry processes.
-            By lowering the barrier of entry, we hope to reach an audience that the municipality was not able to reach before and improve the representation of youth that way.
-            The product we aim to create is a low-entry barrier web platform that enables users to provide input regarding policy in a quick and easy way. This enables youth to engage in policy making, without having to attend meetings, participate in voting procedures and go through gruesome protocols. 
-            This allows the municipality, and collaterally the makers of the Wijkagendas, to collect data on the ideas of the neighborhood in an innovative way. The web platform can be accessed through QR-codes that are placed in areas where youth concentrates.
-            Examples of these places would be schools, playing fields and parks. In order to attract attention to such a QR-code, an attractive poster could be designed that invites people to scan a code and leave behind their opinion. 
-            Additionally, the QR-code system allows for easy geodata collection, which can be useful in analyzing the validity of survey answers.
+      <Infostatic title="Ons concept" content="         
+            Inwoners kunnen een bijdrage leveren aan de leefbaarheid van hun stad. De gemeente Den Haag probeert jongeren en andere inwoners 
+            hiertoe aan te zetten, door ze te benaderen met behulp van wijktafels. 
+            De wijktafel is een goed instrument om burgers te bereiken, maar schiet tekort in het bereiken van jongeren. 
+            Door een vragenlijst in de publieke ruimte aan te bieden met behulp van QR-codes, hopen wij de jongeren die niet naar de wijktafels komen te bereiken.
             ">
       </Infostatic>
   </div>
