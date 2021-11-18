8209e546e335302deddb3e47d739818768cf30f4
Floris Boendermaker <floris.boendermaker@gmail.com>
2021-11-18 17:32:10 +0100

    Disable get method and setting fields to not required

--- app/server/api/models.py
+++ app/server/api/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Feedback(models.Model):
-    age = models.IntegerField()
-    frequency = models.CharField(max_length = 100)
-    reasonOfVisit = models.CharField(max_length = 100)
-    stars = models.CharField(max_length = 100)
-    feedback = models.CharField(max_length = 1000)
-    safety = models.IntegerField()
-    qr_id= models.CharField(max_length = 100)
+    age = models.IntegerField(null=True, blank=True, default=None)
+    frequency = models.CharField(max_length = 100, null=True, blank=True, default=None)
+    reasonOfVisit = models.CharField(max_length = 100, null=True, blank=True, default=None)
+    stars = models.CharField(max_length = 100, null=True, blank=True, default=None)
+    feedback = models.CharField(max_length = 1000, null=True, blank=True, default=None)
+    safety = models.IntegerField(null=True, blank=True, default=None)
+    qr_id= models.CharField(max_length = 100, null=True, blank=True, default=None)
     
     def __str__(self):
         return self.qr_id
--- app/server/api/views.py
+++ app/server/api/views.py
@@ -7,3 +7,4 @@ from .models import Feedback
 class FeedbackViewSet(viewsets.ModelViewSet):
     queryset = Feedback.objects.all().order_by('qr_id')
     serializer_class = HeroSerializer
+    http_method_names = ['post']
