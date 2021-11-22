013fdf6ec4fce5c9354a167a107bfe3b272971f5
Brunohermans <b.hermans@student.tudelft.nl>
2021-11-22 10:31:39 +0100

    added information "about us"

--- app/client/src/views/Contact.vue
+++ app/client/src/views/Contact.vue
@@ -2,6 +2,10 @@
   <div id="contact_info">
     <Infostatic title="Contact" content="Voor vragen over ons project kunt u ons via de mail bereiken:
      b.hermans@student.tudelft.nl"></Infostatic>
+
+     <Infostatic title="Wie zijn wij?" content="Challeges.Social is een initiatief dat tot stand is gekomen
+     door een samenwerking tussen de TU Delft en gemeente Den Haag. Het platform is opgezet door studenten, 
+     en is een pilot om burgerparticipatie te verbeteren." color="secondary"></Infostatic>
   </div>
 </template>
 
