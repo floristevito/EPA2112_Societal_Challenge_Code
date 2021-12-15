4b8d28a0d5c7b187ded465db9d35d1ca930b14f6
Brunohermans <47179528+Brunohermans@users.noreply.github.com>
2021-12-15 11:28:39 +0100

    Added automated poster generation

--- app/server/qr/qr.py
+++ app/server/qr/qr.py
@@ -1,6 +1,12 @@
 import qrcode
+from fpdf import FPDF
 
-for i in range(20):
+
+for i in range(2):
+    fpdf1 = FPDF(orientation = 'L', unit = 'mm', format='A4')
+    fpdf1.add_page()
+    fpdf1.image("../../data/QR_Posters_v2.jpg", x=0, y=0, h=210)
+
     id = 1400 + i
     qr = qrcode.QRCode(
         version=1,
@@ -13,3 +19,6 @@ for i in range(20):
 
     img = qr.make_image(fill_color="black", back_color="white")
     img.save("qr_{}.png".format(id))
+
+    fpdf1.image("qr_{}.png".format(id), x=125, y=40, h=50)
+    fpdf1.output("qr_poster_{}.pdf".format(id), 'F')
