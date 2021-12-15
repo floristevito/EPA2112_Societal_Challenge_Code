0ae5081e2aac1332d31a1bd3bf2c1496851f347d
Floris Boendermaker <floris.boendermaker@gmail.com>
2021-12-15 11:10:42 +0100

    added qrcode generator

--- app/server/qr/qr.py
+++ app/server/qr/qr.py
@@ -1,13 +1,15 @@
 import qrcode
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data('dev.bureaurmc.nl')
-qr.make(fit=True)
+for i in range(20):
+    id = 1400 + i
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data('https://challenges.social/#/form/?qr_id={}'.format(id))
+    qr.make(fit=True)
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("some_file.png")
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qr_{}.png".format(id))
